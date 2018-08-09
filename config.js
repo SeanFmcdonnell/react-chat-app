8cd4977994e8c51a07ff3e38225e6eec60af39dd
Sean McDonnell <sean@freshpow.io>
2018-08-09 19:37:13 -0400

    upload chat app

---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/68d08572-8be4-489e-bca7-4df1b5aea443/token";
+const instanceLocator = "v1:us1:68d08572-8be4-489e-bca7-4df1b5aea443";
+
+export { tokenUrl, instanceLocator }
